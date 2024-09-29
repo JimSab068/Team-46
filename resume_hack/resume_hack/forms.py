5bb44fcb1150aff8b2b63087d584450a320317a5
Sabdha <148603765+JimSab068@users.noreply.github.com>
2024-09-29 08:48:12 -0400

    Delete resume_hack directory

--- resume_hack/resume_hack/forms.py
+++
@@ -1,6 +0,0 @@
-# resume_hack/forms.py
-from django import forms
-
-class ResumeUploadForm(forms.Form):
-    resume_file = forms.FileField(label='Upload Resume (PDF or Word)')
-    job_url = forms.URLField(label='Job URL', required=True)
